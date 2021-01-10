9a88011738bb6c60a2c8fc6f54052cc888a9ca08
Damian Canetti-Rios <dcrios@umich.edu>
2021-01-09 20:35:58 -0600

    blog page added

---
+++ src/templates/blog-page.js
@@ -0,0 +1,95 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+import Button from "react-bootstrap/Button"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPage = ({ data }) => {
+  const hero = data.contentfulPages.hero
+  const title = data.contentfulPages.title
+  const headline = data.contentfulPages.headline
+  const body = data.contentfulPages.body
+  return (
+    <Layout>
+      <SEO
+        title={title}
+        keywords={[
+          `Galveston`,
+          `vacation homes`,
+          `galveston vacation homes`,
+          `galveston rentals`,
+          `galveston airbnbs`,
+          `galveston airbnb`,
+          `places to rent in galveston`,
+          `galveston speakeasy`,
+          `galveston speakeasy cottage`
+        ]}
+      />
+      <h1>{headline}</h1>
+      <h3></h3>
+      <Img fluid={hero.fluid} alt={hero.description} />
+      <div
+        style={{
+          maxWidth: `960px`,
+          margin: `0 auto`,
+          marginLeft: 5,
+          marginRight: 5,
+          marginBottom: 40
+        }}
+      >
+        <MDXRenderer>{body.childMdx.body}</MDXRenderer>
+      </div>
+      <div
+        style={{
+          display: `flex`,
+          justifyContent: `center`,
+          marginBottom: 60,
+          padding: 20
+        }}
+      >
+        <Button
+          variant="outline-danger"
+          size="lg"
+          href="https://www.airbnb.com/rooms/39031320"
+        >
+          Book the Galveston Speakeasy Cottage on Airbnb
+        </Button>
+      </div>
+    </Layout>
+  )
+}
+
+export default BlogPage
+
+export const ContentQuery = graphql`
+  query PageQuery($slug: String!) {
+    contentfulHouseFeaturePost(slug: { eq: $slug }) {
+      id
+      hero: heroImage {
+        fluid {
+          ...GatsbyContentfulFluid_noBase64
+        }
+        description
+      }
+      houseFeatureName
+      shortDescription
+      slug
+      longDescription {
+        childMdx {
+          body
+        }
+      }
+      allImages: allPictures {
+        fluid {
+          ...GatsbyContentfulFluid_noBase64
+        }
+        description
+        id
+      }
+    }
+  }
+`
